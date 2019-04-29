c763ba6b3ecabb8cbd57eb0c93afcfeba44ff3eb
amfoggia <amfoggia@gmail.com>
2019-04-29 17:43:24 +0200

    second lib

---
+++ cool_lib/include/strange_mult.hpp
@@ -0,0 +1,6 @@
+#ifndef __STRANGE_MULT_H
+#define __STRANGE_MULT_H
+
+double strange_mult(std::vector<int> vec1, std::vector<int> vec2);
+
+#endif
---
+++ cool_lib/include/strange_sum.hpp
@@ -0,0 +1,6 @@
+#ifndef __STRANGE_SUM_H
+#define __STRANGE_SUM_H
+
+double strange_sum(std::vector<int> vec1, std::vector<int> vec2);
+
+#endif
