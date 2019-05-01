37aee943768f231b3952966e790704815df10653
amfoggia <amfoggia@gmail.com>
2019-05-01 18:09:21 +0200

    new coollib funcs that use extralib

--- cool_lib/include/max_vec.hpp
+++ cool_lib/include/max_vec.hpp
@@ -2,6 +2,8 @@
 #define __MAX_VEC_H
 
 #include <max.hpp>
+#include <vector>
+#include <stdexcept>
 
 std::vector<int>& max_vec(std::vector<int> vec1, std::vector<int> vec2);
 
--- cool_lib/include/min_vec.hpp
+++ cool_lib/include/min_vec.hpp
@@ -2,6 +2,8 @@
 #define __MIN_VEC_H
 
 #include <min.hpp>
+#include <vector>
+#include <stdexcept>
 
 std::vector<int>& min_vec(std::vector<int> vec1, std::vector<int> vec2);
 
